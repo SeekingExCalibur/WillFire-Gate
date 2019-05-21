b572df1c3c457b09adc7d6b6d105d3bb9ae9fac5
Joseph <joedhamlett@gmail.com>
2019-05-21 18:58:12 -0400

    Initial project push with basic tree of scenes and assets.
    Adding current Unity project and concept art for first time.
    
    Scenes added: Title Scene, D-Day scene 1, Temple scene 1, Reaper difficulty selector.
    
    Assets added include: RPG character controller with scripts, models and prefabs. Polygon Dungeon pack and Polygon War pack.
    
    test rigging characters and scripting npc's are currently in temple scene.

---
+++ 2019WillFire_Gate/2019SummerVR/Assets/Scripts/slayerOneController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class slayerOneController : MonoBehaviour {
+    public Transform target;
+    private NavMeshAgent myNavAgent;
+    private Animator myAnim;
+
+	// Use this for initialization
+	void Start () {
+        myNavAgent = GetComponent<NavMeshAgent>();
+        myAnim = GetComponent<Animator>();
+        GoToPoint(target.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(myNavAgent.speed);
+        myAnim.SetFloat("Speed", myNavAgent.velocity.magnitude);
+	}
+
+    public void GoToPoint(Vector3 target) {
+        myNavAgent.SetDestination(target);
+    }
+
+}
